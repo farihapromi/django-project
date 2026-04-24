988030d46f7d48d8e3679511d0573fc6e336bf19
Fariha Promi <farihapromi2611@gmail.com>
2026-04-24 23:15:33 +0600

    Update settings'

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'opitons': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLE'"
+        }
+
     }
 }
 
